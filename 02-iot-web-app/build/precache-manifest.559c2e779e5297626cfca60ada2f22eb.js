f11c491800ecaa40a3f3a9a220b07244807d3dd6
Fedyashev Andrei <fedjashev@gmail.com>
2018-12-30 15:02:06 +0300

    Build front project and a add static files to server

---
+++ 02-iot-web-app/build/precache-manifest.559c2e779e5297626cfca60ada2f22eb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "31076bd76d80db5c0a1c",
+    "url": "/static/js/main.31076bd7.chunk.js"
+  },
+  {
+    "revision": "40f602e43774dbd1d982",
+    "url": "/static/js/1.40f602e4.chunk.js"
+  },
+  {
+    "revision": "40f602e43774dbd1d982",
+    "url": "/static/css/1.dd3aba94.chunk.css"
+  },
+  {
+    "revision": "854d423e919168f2118387ce76273a61",
+    "url": "/index.html"
+  }
+];
